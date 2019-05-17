5ab0fdd210d5966240dcefe954bc036f089eb949
fubar2 <ross.lazarus@gmail.com>
2019-05-17 22:45:06 +1000

    added optional parameter - input filename - to loadcellplot.py

--- loadcellplot.py
+++ loadcellplot.py
@@ -1,31 +1,43 @@
 #! /usr/bin/python3
 # stand alone plotter - currently just uses the growing output file
 # but can be used on historical files by editing
+# adjusted to accept an input xls file
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import datetime
+import time
+import sys
+import os
 from dateutil import tz
 from tzlocal import get_localzone
+
 tzl = get_localzone().zone
-iname = 'loadcell.xls' # 'loadcell_first17hours_4kg.xls'
+mdates.rcParams['timezone'] = tzl
+
+if (len(sys.argv) > 1):
+    iname = sys.argv[1]
+    ifilename, ifile_extension = os.path.splitext(iname)
+    imname = '%s.png' % ifilename 
+else:
+    iname = 'loadcell.xls' # 'loadcell_first17hours_4kg.xls'
+    imname = 'loadcell_%s.png' % (time.strftime('%Y%m%d%H%M%S',time.localtime()))
+
 df = pd.read_csv(iname,sep='\t')
+
 df.columns=["epoch","mass"]
-df['timestamp'] = pd.to_datetime(list(df['epoch']),unit='s')
-df['date'] = pd.to_datetime(df['epoch'],unit='s',utc=True)
+df['date'] = pd.to_datetime(df['epoch'],unit='s')
 df.set_index(df['date'],inplace=True)
 df = df.tz_localize(tz=tzl)
 
-mdates.rcParams['timezone'] = tzl
+lastone = df.epoch[-1]
+lasttime = time.strftime('%Y%m%d_%H%M%S',time.localtime(lastone))
 
+firstone = df.epoch[0]
+firsttime = time.strftime('%Y%m%d_%H%M%S',time.localtime(firstone))
 
-lastone = df.iloc[-1].tolist()
-lasttime = lastone[2].strftime('%Y%d%m_%H%M%S')
-firstone = df.iloc[1].tolist()
-firsttime = firstone[2].strftime('%Y%d%m_%H%M%S')
-imname = 'loadcell%s.png' % (lasttime)
 x = df['date']
 y = df['mass']
 ms = 2
