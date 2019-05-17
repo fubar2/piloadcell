acbaa141d05d195de40fd11ae96b0e966e9bc6b8
fubar2 <ross.lazarus@gmail.com>
2019-05-18 09:43:09 +1000

    Updated to trim +/- 2SD before plotting because we get occasional single results that appear to be real outliers and end up distorting the Y axis.
    3SD works for larger datasets. Interesting that the pattern I'm seeing has some
    obvious low frequency variation - 2-4 hourly period so better suited to an
    autoregressive filter - will explore.

--- loadcellplot.py
+++ loadcellplot.py
@@ -16,7 +16,23 @@ from tzlocal import get_localzone
 
 tzl = get_localzone().zone
 mdates.rcParams['timezone'] = tzl
+trimci = 2.0
 
+def trimcl(df,nsd):
+    mene = df.mass.mean()
+    ci = df.mass.std()*2.0
+    ucl = mene + ci
+    lcl = mene - ci
+    notbig = df.mass < ucl
+    df2 = df[notbig]
+    notsmall = df2.mass > lcl
+    df2 = df2[notsmall]
+    nhi = sum(notbig==False)
+    nlo = sum(notsmall==False)
+    s = 'Trim +/- %.1f SD removed %d above %.2f and %d below %.2f\n' % (nsd,nhi,ucl,nlo,lcl)
+    s2 = '##Before trim:\n %s\nAfter trim:\n %s' % (df.describe(),df2.describe())
+    return(df2,s,s2)
+    
 if (len(sys.argv) > 1):
     iname = sys.argv[1]
     ifilename, ifile_extension = os.path.splitext(iname)
@@ -31,7 +47,9 @@ df.columns=["epoch","mass"]
 df['date'] = pd.to_datetime(df['epoch'],unit='s')
 df.set_index(df['date'],inplace=True)
 df = df.tz_localize(tz=tzl)
-
+if trimci:
+    df,note,descr = trimcl(df,trimci)
+print(descr)
 lastone = df.epoch[-1]
 lasttime = time.strftime('%Y%m%d_%H%M%S',time.localtime(lastone))
 
@@ -50,7 +68,12 @@ elif nrow > 100000:
     ms = 0.2
 plt.figure(figsize=(10,8))
 plt.plot(x, y, c='blue',linestyle='None', markersize = ms, marker='o')
-plt.title('%d Loadcell values, %s to %s' % (nrow,firsttime,lasttime))
+titl = '%d Loadcell values, %s to %s' % (nrow,firsttime,lasttime)
+if trimci:
+    plt.title(note,fontsize=14)
+    plt.suptitle(titl,fontsize=18, y=0.975)
+else:
+    plt.title(titl)
 plt.xlabel('Time (usually as month-day hour)')
 plt.ylabel('Reported Mass (g)')
 
--- loadcellplotter.py
+++ loadcellplotter.py
@@ -13,6 +13,23 @@ from tzlocal import get_localzone
 import time
 
 tzl = get_localzone().zone
+trimci = 2.0 # set None for no trim
+
+def trimcl(df,nsd):
+    mene = df.mass.mean()
+    ci = df.mass.std()*2.0
+    ucl = mene + ci
+    lcl = mene - ci
+    notbig = df.mass < ucl
+    df2 = df[notbig]
+    notsmall = df2.mass > lcl
+    df2 = df2[notsmall]
+    nhi = sum(notbig==False)
+    nlo = sum(notsmall==False)
+    s = 'Trim +/- %.1f SD removed %d above %.2f and %d below %.2f\n' % (nsd,nhi,ucl,nlo,lcl)
+    s2 = '##Before trim:\n %s\nAfter trim:\n %s' % (df.describe(),df2.describe())
+    return(df2,s,s2)
+
 
 def loadcellplot():
     df = pd.read_csv('loadcell.xls',sep='\t')
@@ -20,6 +37,8 @@ def loadcellplot():
     df['date'] = pd.to_datetime(df['epoch'],unit='s')
     df.set_index(df['date'],inplace=True)
     df = df.tz_localize(tz=tzl)
+    if trimci:
+        df,note,descr = trimcl(df,trimci)
     mdates.rcParams['timezone'] = tzl
     lastone = df.epoch[-1] # easier to use the original epoch rather than the internal datetimes!
     lasttime = time.strftime('%Y%m%d_%H%M%S',time.localtime(lastone))
@@ -38,7 +57,12 @@ def loadcellplot():
         ms = 0.2
     plt.figure(figsize=(10,8))
     plt.plot(x, y, c='blue',linestyle='None', markersize = ms, marker='o')
-    plt.title('%d Loadcell values, %s to %s' % (df.shape[0],firsttime,lasttime))
+    titl = '%d Loadcell values from %s to %s' % (df.shape[0],firsttime,lasttime)
+    if trimci:
+        plt.title(note,fontsize=14)
+        plt.suptitle(titl,fontsize=18, y=0.975)
+    else:
+        plt.title(titl)
     plt.xlabel('Date/Time (month-day hour for example)')
     plt.ylabel('Reported Mass (g)')
     plt.grid()
